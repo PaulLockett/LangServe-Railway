82590b7ff8769261d62865675abf1ea99fb1805b
Paul Lockett <plockett@flexport.com>
2023-11-20 22:54:18 -0800

    add health and root route

--- app/server.py
+++ app/server.py
@@ -7,6 +7,19 @@ app = FastAPI()
 
 add_routes(app, pirate_speak_chain, path="/pirate-speak")
 
+
+@app.get("/")
+def root():
+    return {
+        "message": "Welcome to the langserve server! More information and routes can be found at /docs. 🦜 🏓",
+    }
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
+
 if __name__ == "__main__":
     import uvicorn
 
